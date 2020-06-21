c5921ed82d65c2c85d9583e08586868c6b6a86b8
Paweł Niesiołowski <pavel.niesiolowski@gmail.com>
2020-06-21 15:22:08 +0200

    Change database url environment name

--- config/__init__.py
+++ config/__init__.py
@@ -19,7 +19,7 @@ class Config:
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
