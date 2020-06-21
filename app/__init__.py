34dadfadf0ac409a47a23b3c300823ce491943f7
Paweł Niesiołowski <pavel.niesiolowski@gmail.com>
2020-06-21 14:43:30 +0200

    Add basic CORS mechanism

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from config import create_config_for_environment
 from app.code_review import setup_module as setup_code_review
 from app.db import db, init_db
@@ -7,6 +8,7 @@ from app.errors import setup_errors
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
 
     if config is None:
         config = create_config_for_environment()
